53a378da8f3b0710f7e2fa58c39ec8ec86dfe49a
Steph Samson <ayo@kasteph.com>
2024-10-15 00:39:35 +0900

    address comments from dennis

--- db/migrations/000017_create_requests_dnf_table.up.sql
+++ db/migrations/000017_create_requests_dnf_table.up.sql
@@ -3,27 +3,27 @@ BEGIN;
 CREATE TABLE requests_denormalized
 (
     -- An internal unique id that identifies a crawl.
-    id              INT GENERATED ALWAYS AS IDENTITY,
+    id                       BIGINT GENERATED ALWAYS AS IDENTITY,
     -- Timestamp of when this request started.
-    timestamp       TIMESTAMPTZ NOT NULL,
+    request_started_at       TIMESTAMPTZ NOT NULL,
     -- The message type of this request
-    request_type    message_type NOT NULL,
+    request_type             message_type NOT NULL,
     -- Peer ID of the ant doing the request,
-    ant_id          TEXT NOT NULL,
+    ant_multihash            TEXT NOT NULL,
     -- The peer related to this request
-    peer_id         TEXT NOT NULL,
-    -- The key ID of this request (?)
-    key_id          TEXT NOT NULL,
-    -- An array of all multi address IDs of the remote peer.
-    multi_address_ids TEXT[],
+    peer_multihash           TEXT NOT NULL,
+    -- The key of this request
+    key_multihash            TEXT NOT NULL,
+    -- An array of all multi addresses of the remote peer.
+    multi_addresses          TEXT[],
 
-    agent_version TEXT,
+    agent_version            TEXT,
 
-    normalized_at TIMESTAMPTZ,
+    normalized_at            TIMESTAMPTZ,
 
-    PRIMARY KEY (id, timestamp)
-) PARTITION BY RANGE (timestamp);
+    PRIMARY KEY (id, request_started_at)
+) PARTITION BY RANGE (request_started_at);
 
-CREATE INDEX idx_requests_dnf_timestamp ON requests (timestamp);
+CREATE INDEX idx_requests_dnf_timestamp ON requests_denormalized (request_started_at);
 
 COMMIT;
