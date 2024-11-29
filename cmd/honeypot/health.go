a6db66b8de94897ef7b2869c57ad282ce123f04b
Steph Samson <ayo@kasteph.com>
2024-11-30 00:03:42 +0900

    address comments from dennis

--- cmd/honeypot/health.go
+++ cmd/honeypot/health.go
@@ -1,25 +1,33 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"net/http"
 	"os"
+
+	"github.com/urfave/cli/v2"
 )
 
-func HealthCheck(ctx *context.Context) error {
+func HealthCheck(c *cli.Context) error {
 	endpoint := fmt.Sprintf(
 		"http://%s:%s/health",
 		os.Getenv("METRICS_HOST"),
 		os.Getenv("METRICS_PORT"),
 	)
-	resp, err := http.Get(endpoint)
+	req, err := http.NewRequestWithContext(c, http.MethodGet, endpoint, nil)
 	if err != nil {
 		return err
 	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
 	if resp.StatusCode == http.StatusOK {
 		return nil
 	}
 
-	return fmt.Errorf("unhealthy")
+	return fmt.Errorf("unhealthy: status code %d", resp.StatusCode)
 }
