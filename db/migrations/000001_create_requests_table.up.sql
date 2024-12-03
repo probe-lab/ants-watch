d0cf83fda640a002affb74199799effdc6276f7d
Dennis Trautwein <git@dtrautwein.eu>
2024-12-03 16:04:33 +0100

    increase ttl and change merge tree engine to clustered

--- db/migrations/000001_create_requests_table.up.sql
+++ db/migrations/000001_create_requests_table.up.sql
@@ -10,6 +10,6 @@ CREATE TABLE requests
     request_type     String,
     key_multihash    String,
     multi_addresses  Array(String)
-) ENGINE = MergeTree
+) ENGINE = ReplicatedMergeTree
     PRIMARY KEY (started_at)
-TTL toDateTime(started_at) + INTERVAL 1 DAY;
+TTL toDateTime(started_at) + INTERVAL 180 DAY;
