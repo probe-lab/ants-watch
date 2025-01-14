c21d59b60f1a723e65ce5b3d365a1b5135fb185b
Dennis Trautwein <git@dtrautwein.eu>
2025-01-14 14:35:07 +0100

    move: utils code to keys related code

--- keys_test.go
+++ keys_test.go
@@ -106,3 +106,25 @@ func TestReturnKeysToEmptyTrie(t *testing.T) {
 	require.True(t, found)
 	require.Equal(t, key, foundKey)
 }
+
+func TestBitstrToBit256(t *testing.T) {
+	strKey := bitstr.Key("00001111") // 0x0f
+	var padding [bit256.KeyLen]byte
+	got := bitstrToBit256(strKey, padding[:])
+
+	var target [bit256.KeyLen]byte
+	target[0] = 0x0f
+	want := bit256.NewKey(target[:])
+	require.Equal(t, want, got)
+
+	strKey = "000011110000" // 0x0f0
+	got = bitstrToBit256(strKey, padding[:])
+	require.Equal(t, want, got)
+
+	strKey = "111" // 0xe
+	padding[0] = 0x0f
+	got = bitstrToBit256(strKey, padding[:])
+	target[0] = 0xef
+	want = bit256.NewKey(target[:])
+	require.Equal(t, want, got)
+}
