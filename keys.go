1d5f8415678cdb0e01a018c81d77b4f693e0ed57
Dennis Trautwein <git@dtrautwein.eu>
2025-01-14 14:35:07 +0100

    move: utils code to keys related code

--- keys.go
+++ keys.go
@@ -7,8 +7,12 @@ import (
 	"io"
 	"os"
 
+	"github.com/ipfs/go-cid"
 	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/core/peer"
+	mh "github.com/multiformats/go-multihash"
+	mhreg "github.com/multiformats/go-multihash/core"
+
 	"github.com/probe-lab/go-libdht/kad/key"
 	"github.com/probe-lab/go-libdht/kad/key/bit256"
 	"github.com/probe-lab/go-libdht/kad/key/bitstr"
@@ -180,4 +184,43 @@ func (db *KeysDB) MatchingKeys(prefixes []bitstr.Key, returned []crypto.PrivKey)
 	db.writeKeysToFile(keysTrie)
 
 	return keys
+}
+
+// PeerIDToKadID converts a libp2p peer.ID to its binary kademlia identifier
+func PeerIDToKadID(pid peer.ID) bit256.Key {
+	hasher, err := mhreg.GetHasher(mh.SHA2_256)
+	if err != nil {
+		panic(err)
+	}
+	hasher.Write([]byte(pid))
+	return bit256.NewKey(hasher.Sum(nil))
+}
+
+func NsToCid(ns string) (cid.Cid, error) {
+	h, err := mh.Sum([]byte(ns), mh.SHA2_256, -1)
+	if err != nil {
+		return cid.Undef, err
+	}
+
+	return cid.NewCidV1(cid.Raw, h), nil
+}
+
+func bitstrToBit256(strKey bitstr.Key, padding []byte) bit256.Key {
+	bit256Key := make([]byte, 32)
+	copy(bit256Key, padding)
+
+	var currByte byte
+	i := 0
+	for ; i < strKey.BitLen(); i++ {
+		currByte = currByte | (byte(strKey.Bit(i)) << (7 - (i % 8)))
+		if i%8 == 7 {
+			bit256Key[i/8] = currByte
+			currByte = 0
+		}
+	}
+	if i%8 != 0 {
+		currByte = currByte | bit256Key[i/8]<<(7-(i%8))>>(7-(i%8))
+		bit256Key[i/8] = currByte
+	}
+	return bit256.NewKey(bit256Key)
 }
